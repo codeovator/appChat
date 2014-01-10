f69b135bd07298b76801a62b6c78295fd91cac64
ranjana maurya <ranjana.d.maurya@gmail.com>
2014-01-11 04:18:57 +0530

    updated map ll

--- map.php
+++ map.php
@@ -14,10 +14,10 @@
 					var lat="<?php echo $result['longitude']?>";
 					var locations = [
 					[name, lang, lat , 4],
-					['Coogee Beach', -33.923036, 151.259052, 5],
-					['Cronulla Beach', -34.028249, 151.157507, 3],
-					['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-					['Maroubra Beach', -33.950198, 151.259302, 1]
+					['Coogee Beach', lang+2, lat+2, 5],
+					['Cronulla Beach', lang+4, lat+4, 3],
+					['Manly Beach', lang+6, lat+6, 2],
+					['Maroubra Beach', lang+8, lat+8, 1]
 					];
 
 					var map = new google.maps.Map(document.getElementById('map-div'), {
